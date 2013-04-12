82100547a42d13021d5435209861f48584e47d2a
Sravan Bhamidipati <bsravanin@gmail.com>
2013-04-12 15:52:18 -0400

    Empty parsing rules.

--- frontend/lexer.py
+++ frontend/lexer.py
@@ -14,6 +14,7 @@
 
 from ply import lex
 from ply.lex import TOKEN
+from ply import yacc
 
 
 class Error(Exception):
@@ -38,9 +39,9 @@ reserved = {
     'elif': 'ELIF',
     'else': 'ELSE',
     'for': 'FOR',
-    'from': 'FROM',
+    # 'from': 'FROM',
     'if': 'IF',
-	'import': 'IMPORT',
+	# 'import': 'IMPORT',
     'in': 'IN',
     'is': 'IS',
     'not': 'NOT',
@@ -48,7 +49,7 @@ reserved = {
     'print': 'PRINT',
     'return': 'RETURN',
     'while': 'WHILE',
-	'None': 'NONE',
+	# 'None': 'NONE',
 }
 
 
@@ -274,5 +275,373 @@ def lexer_test(filename):
 		# print output
 		print tok
 
+
+# ----------------------------------------------------------------------
+# Parser begins now that tokenizing is done.
+# ----------------------------------------------------------------------
+
+def p_start(p):
+	'''start : ENDMARKER
+			 | content_list ENDMARKER'''
+	pass
+
+
+def p_content_list(p):
+	'''content_list : content
+					| content_list content'''
+	pass
+
+
+def p_content(p):
+	'''content : NEWLINE
+			   | top_level_stmt'''
+	pass
+
+
+def p_function_def(p):
+	'''function_def : DEF NAME NAME LPAREN RPAREN COLON suite
+					| DEF NAME NAME LPAREN parameter_list RPAREN COLON suite'''
+	pass
+
+
+def p_parameter_list(p):
+	'''parameter_list : param
+					  | parameter_list COMMA param'''
+	pass
+
+
+def p_param(p):
+	'''param : NAME NAME'''
+	pass
+
+
+def p_top_level_stmt(p):
+	'''top_level_stmt : stmt
+					  | function_def
+					  | class_def'''
+	pass
+
+
+def p_stmt(p):
+	'''stmt : simple_stmt
+			| compound_stmt'''
+	pass
+
+
+def p_simple_stmt(p):
+	'''simple_stmt : small_stmt NEWLINE
+				   | declaration'''
+	pass
+
+
+def p_small_stmt(p):
+	'''small_stmt : expr
+				  | assignment_stmt
+				  | print_stmt
+				  | flow_stmt'''
+	pass
+
+
+def p_assignment_stmt(p):
+	'''assignment_stmt : NAME target ASSIGN expr'''
+	pass
+
+
+def p_target(p):
+	'''target : NAME
+			  | attribute_ref
+			  | subscription'''
+	pass
+
+
+
+# def p_target_list(p):
+	'''target_list : target
+				   | target_list COMMA target'''
+	# pass
+
+
+def p_print_stmt(p):
+	'''print_stmt : PRINT
+				  | PRINT expr_list'''
+	pass
+
+
+def p_flow_stmt(p):
+	'''flow_stmt : BREAK
+				 | CONTINUE
+				 | RETURN
+				 | RETURN expr'''
+	pass
+
+
+def p_declaration(p):
+	'''declaration : NAME name_list NEWLINE'''
+	pass
+
+
+def p_name_list(p):
+	'''name_list : NAME
+				 | name_list COMMA NAME'''
+	pass
+
+
+def p_compound_stmt(p):
+	'''compound_stmt : if_stmt
+					 | while_stmt
+					 | for_stmt'''
+	pass
+
+
+def p_if_stmt(p):
+	'''if_stmt : IF expr COLON suite
+			   | IF expr COLON suite else_clause
+			   | IF expr COLON suite elif_clauses else_clause'''
+	pass
+
+
+def p_elif_clauses(p):
+	'''elif_clauses : elif_clause
+					| elif_clauses elif_clause'''
+	pass
+
+
+def p_elif_clause(p):
+	'''elif_clause : ELIF expr COLON suite'''
+	pass
+
+
+def p_else_clause(p):
+	'''else_clause : ELSE expr COLON suite'''
+	pass
+
+
+def p_while_stmt(p):
+	'''while_stmt : WHILE expr COLON suite'''
+	pass
+
+
+def p_for_stmt(p):
+	'''for_stmt : FOR NAME NAME IN primary COLON suite'''
+	pass
+
+
+def p_suite(p):
+	'''suite : simple_stmt
+			 | NEWLINE INDENT stmt_list DEDENT'''
+	pass
+
+
+def p_stmt_list(p):
+	'''stmt_list : stmt
+				 | stmt_list stmt'''
+	pass
+
+
+def p_expr_list(p):
+	'''expr_list : expr
+				 | expr_list COMMA expr'''
+	pass
+
+
+def p_expr(p):
+	'''expr : or_test''' 
+	pass
+
+
+def p_or_test(p):
+	'''or_test : and_test
+			   | or_test OR and_test'''
+	pass
+
+
+def p_and_test(p):
+	'''and_test : not_test
+				| and_test AND not_test'''
+	pass
+
+
+def p_not_test(p):
+	'''not_test : comparison
+				| NOT not_test'''
+	pass
+
+
+def p_comparison(p):
+	'''comparison : arith_expr
+				  | comparison comp_op arith_expr'''
+	pass
+
+
+def p_comp_op(p):
+	'''comp_op : LESS
+			   | LESSEQUAL
+			   | GREATER
+			   | GREATEREQUAL
+			   | EQUAL
+			   | NOTEQUAL
+			   | IN
+			   | NOT IN
+			   | IS
+			   | NOT IS'''
+	pass
+
+
+def p_arith_expr(p):
+	'''arith_expr : mult_expr
+				  | arith_expr arith_op mult_expr'''
+	pass
+
+
+def p_arith_op(p):
+	'''arith_op : PLUS
+				| MINUS'''
+	pass
+
+
+def p_mult_expr(p):
+	'''mult_expr : unary_expr
+				 | mult_expr mult_op unary_expr'''
+	pass
+
+
+def p_mult_op(p):
+	'''mult_op : STAR
+			   | SLASH
+			   | PERCENT'''
+	pass
+
+
+def p_unary_expr(p):
+	'''unary_expr : power
+				  | unary_op unary_expr'''
+	pass
+
+
+def p_unary_op(p):
+	'''unary_op : PLUS
+				| MINUS'''
+	pass
+
+
+def p_power(p):
+	'''power : primary
+			 | primary DOUBLESTAR unary_expr'''
+	pass
+
+
+def p_primary(p):
+	'''primary : atom
+			   | attribute_ref
+			   | subscription
+			   | call'''
+	pass
+
+
+def p_atom(p):
+	'''atom : NAME
+			| literal
+			| enclosure'''
+	pass
+
+
+def p_literal(p):
+	'''literal : string_list
+			   | NUMBER'''
+	pass
+
+
+def p_string_list(p):
+	'''string_list : STRING
+				   | string_list STRING'''
+	pass
+
+
+def p_enclosure(p):
+	'''enclosure : LPAREN expr RPAREN
+				 | LBRACKET expr_list RBRACKET
+				 | LBRACE key_datum_list RBRACE
+				 | LBRACE expr_list RBRACE'''
+	pass
+
+
+def p_key_datum_list(p):
+	'''key_datum_list : key_datum
+					  | key_datum_list COMMA key_datum'''
+	pass
+
+
+def p_key_datum(p):
+	'''key_datum : expr COLON expr'''
+	pass
+
+
+def p_attribute_ref(p):
+	'''attribute_ref : primary DOT NAME'''
+	pass
+
+
+def p_subscription(p):
+	'''subscription : primary LBRACKET expr RBRACKET'''
+	pass
+
+
+# TODO
+def p_call(p):
+	'''call : primary LPAREN argument_list RPAREN'''
+	pass
+
+
+def p_argument_list(p):
+	'''argument_list : expr
+					 | argument_list COMMA expr'''
+	pass
+
+
+def p_class_def(p):
+	'''class_def : CLASS NAME LPAREN RPAREN COLON class_def_suite
+				 | CLASS NAME LPAREN NAME RPAREN COLON class_def_suite'''
+	pass
+
+
+def p_class_def_suite(p):
+	'''class_def_suite : NEWLINE INDENT class_stmt_list DEDENT'''
+	pass
+
+
+def p_class_stmt_list(p):
+	'''class_stmt_list : function_defs
+					   | declarations function_defs
+					   | node_types_def function_defs
+					   | declarations node_types_def function_defs'''
+	pass
+
+
+def p_declarations(p):
+	'''declarations : declaration
+					| declarations declaration'''
+	pass
+
+
+def p_node_types_def(p):
+	'''node_types_def : assignment_stmt NEWLINE'''
+	pass
+
+
+def p_function_defs(p):
+	'''function_defs : function_def
+					 | function_defs function_def'''
+	pass
+
+
+def p_error(p):
+	print "Syntax error in", p
+
+
+parser = yacc.yacc()
+
+
+'''
 import sys
 lexer_test(sys.argv[1])
+'''
