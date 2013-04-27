85371059f9a0ce7f7adaf0a8e4bc7dcd98ec6598
Michael Deeringer <mdeeringer@gmail.com>
2013-04-27 14:03:39 -0400

    util.py: new util file for the frontend

---
+++ frontend/util.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+
+'Utility module for the Gramola frontend.'
+
+import os
+
+
+BUILTINS_FILENAME = os.path.abspath('builtins.gr')
+# NOTE: this must match the class declaration in builtins.gr
+BUILTINS_CLASS_NAME = '__builtins'
+CONSTRUCTOR_NAME = '__init__'
