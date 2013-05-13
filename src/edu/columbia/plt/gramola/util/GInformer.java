1d481925ef84a91dadf86ebc440771edbac94983
Mike FH Su <mikefhsu@dhcp64.cs.columbia.edu>
2013-05-13 14:17:09 -0400

    Change msg title from Glib to Gramola

--- src/edu/columbia/plt/gramola/util/GInformer.java
+++ src/edu/columbia/plt/gramola/util/GInformer.java
@@ -7,7 +7,7 @@ package edu.columbia.plt.gramola.util;
  */
 public class GInformer {
 	
-	private static String gHead = "[Glib]: ";
+	private static String gHead = "[Gramola]: ";
 	
 	public static void printMessage(String glibMsg) {
 		System.out.println(gHead + glibMsg);
